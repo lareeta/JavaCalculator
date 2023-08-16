e8722769331b5435207fb1cceefe330bc61b212b
ritaRakhimova <51830833+lapari@users.noreply.github.com>
2023-08-16 13:11:13 +0300

    Create butterfly and simple loop

---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 3; i >= 1; i--) {
+            System.out.println(i);
+        };
+        System.out.println("Поехали!!!");
+
+    }
+}
